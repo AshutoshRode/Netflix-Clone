a69c83db05c71d3f475b14fd7cc8ebc2edc872d1
Ashutosh Vilas Rode <117566964+AshutoshRode@users.noreply.github.com>
2025-10-21 10:09:04 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,20 @@
-// import firebase from './firebase'; 
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/database";
 
 const firebaseConfig = {
-apiKey: "AIzaSyAvxW90mXzFRU8ehpY4KhM6pa2AIw4eG5c",
+  apiKey: "AIzaSyAvxW90mXzFRU8ehpY4KhM6pa2AIw4eG5c",
   authDomain: "netflix-clone-51bfe.firebaseapp.com",
   projectId: "netflix-clone-51bfe",
-  storageBucket: "netflix-clone-51bfe.firebasestorage.app",
+  storageBucket: "netflix-clone-51bfe.appspot.com", 
   messagingSenderId: "423929070050",
   appId: "1:423929070050:web:ec29589807a72034a8b9bb"
 };
-const firebaseApp = firebase.initializeApp(firebaseConfig);
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Export only what you need
 const auth = firebase.auth();
 
 export default auth;
