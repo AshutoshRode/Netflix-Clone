3faf641be6f31c27b6085c3b597ce354bdf4db7e
Ashutosh Vilas Rode <117566964+AshutoshRode@users.noreply.github.com>
2025-10-21 10:15:05 +0530

    Update Row.js

--- src/component/Row.js
+++ src/component/Row.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import "../css/Row.css";
 import axios from '../localAxios.js';
-function Row({ title, isLargeRow, fetchURL }) {
 
+function Row({ title, isLargeRow, fetchURL }) {
   const [movies, setMovie] = useState([]);
 
   const base_url = "https://image.tmdb.org/t/p/original/";
@@ -10,29 +10,28 @@ function Row({ title, isLargeRow, fetchURL }) {
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchURL);
-
       setMovie(request.data.results);
-      return request
+      return request;
     }
     fetchData();
-  },
-    [fetchURL])
+  }, [fetchURL]);
+
   return (
-    <div className='row' >
+    <div className="row">
       <h2>{title}</h2>
 
-      <div className='row_posters'>
-        {
-          movies.map(movie => {
-            return <img src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} className={`rowposter ${isLargeRow && "largeImage"}`} keys={movie.id} />
-
-          })
-        }
-
+      <div className="row_posters">
+        {movies.map(movie => (
+          <img
+            key={movie.id} // ✅ corrected key prop
+            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+            alt={movie?.name || movie?.title || "Movie poster"} // ✅ added alt prop
+            className={`rowposter ${isLargeRow && "largeImage"}`}
+          />
+        ))}
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Row
+export default Row;
